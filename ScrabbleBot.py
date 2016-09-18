1335ea1b2d91bae2eadbdd8eb5375be43a5db43f
Rebecca Watson <Rebecca Watson>
2016-09-18 14:54:27 -0600

    parses all files

---
+++ ScrabbleBot.py
@@ -0,0 +1,15 @@
+
+from ScrabblePlayer import *
+
+
+# OUR BOT GOES HERE
+
+# self.rack is an instance variable for the bot's current rack of tiles
+
+class ScrabbleBot(ScrabblePlayer):
+
+    # Need to return a dictionary of (row, col):letter pairs
+    # Input board is a ScrabbleBoard object
+    def chooseMove(self, board):
+        move = {}
+       	return chooseMove
