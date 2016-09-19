e52f225de8720133dc73270070cc264f43977439
jonahbroh <thepigking@gmail.com>
2016-09-18 18:39:54 -0600

    added temp weights

--- play_bot_vs_bot.py
+++ play_bot_vs_bot.py
@@ -1,9 +1,11 @@
 from ScrabbleGame import *
 from ScrabblePlayer import *
 from ScrabbleBot import *
+from ScrabbleBotter import *
 
 if __name__ == "__main__":
     sg = ScrabbleGame(BOARD_SIZE)
-    player1 = ScrabbleBot(sg.drawTiles(RACK_MAX_SIZE), sg)
-    player2 = ScrabbleBot(sg.drawTiles(RACK_MAX_SIZE), sg)
+    weights = [.1, 2.0, -.5, -2, .1, 4.0, 0, .95, 10, 1]
+    player1 = ScrabbleBotter(sg.drawTiles(RACK_MAX_SIZE), sg, weights)
+    player2 = ScrabbleBotter(sg.drawTiles(RACK_MAX_SIZE), sg, weights)
     sg.playGame(player1, player2)
