f68b38c70b1cd8c73c96aac89beecb9fa2f2d55f
ChaoTC <zacharyeberhart@gmail.com>
2016-09-18 23:57:01 -0600

    Seems to work- doesn't crash at the epoch and writes files

--- ScrabbleGA.py
+++ ScrabbleGA.py
@@ -10,12 +10,11 @@ from ScrabblePlayer import *
 from scrabble_globals import *
 from ScrabbleBotter import *
 
-length = 10 #length of genes
+length = 11 #length of genes
 
 class GA:
     def __init__(self):
         self.bots = [] #list of genes? (doubles)
-        #self.weights = []
         self.fitnesses = []
 
     def generatePopulationFitness(self):
@@ -24,28 +23,18 @@ class GA:
         scores = []
         mybots = []
         for i in range(pop_size): scores.append(0) #initializes list
-        for p1 in range(pop_size):
-            for j in range(1, pop_size):
+        for p1 in range(pop_size-1):
+            for p2 in range(p1+1, pop_size):
                 sGame = ScrabbleGame(BOARD_SIZE)
-                # print p1
-                p2 = (p1+j)%pop_size
-                # print p2
-                # print self.bots
+                
                 bot1 = ScrabbleBotter(sGame.drawTiles(RACK_MAX_SIZE), sGame, self.bots[p1])
                 bot2 = ScrabbleBotter(sGame.drawTiles(RACK_MAX_SIZE), sGame, self.bots[p2])
                 sg = sGame.playGame(bot1, bot2)
-                # print sg[0], sg[1]
-                # print sg[0] - sg[1]
-                # print scores[p1] + (sg[0] - sg[1])
+                
                 scores[p1] = scores[p1] + (sg[0] - sg[1])
                 scores[p2] += sg[1] - sg[0]
-                # print "score ", scores
-
-        # print "scores ", scores
+                
         for ind in range(len(scores)):
-            print ind
-            print self.fitnesses
-            print scores
             self.fitnesses[ind] = scores[ind] * 1/pop_size
             print ind, scores[ind], self.fitnesses[ind]
 
@@ -63,11 +52,10 @@ class GA:
 
 
     def pickFitParent(self):
-        # print self.fitnesses
+        theMin = abs(min(self.fitnesses))
         for i in range(len(self.fitnesses)):
-            self.fitnesses[i] += (abs(min(self.fitnesses)) + 1) #pushing them all above 0
-        total_fitnesses = sum(self.fitnesses)
-        # print self.fitnesses
+            self.fitnesses[i] += theMin #pushing them all above 0
+        total_fitnesses = round(sum(self.fitnesses))
         r = random.randrange(total_fitnesses)
         ind = -1
         while r > 0:
@@ -78,11 +66,9 @@ class GA:
     def pickBestN(self, n):
         bestGenomes = []
         for i in range(n):
-            # print "?"
             print self.fitnesses
             max_value = max(self.fitnesses)
             max_index = self.fitnesses.index(max_value)
-            # print max_index, "max index"
             print self.bots[max_index]
             bestGenomes.append(self.bots.pop(max_index)) #remove best from population
             self.bots.append(0)
@@ -94,7 +80,6 @@ class GA:
         baby = []
         for i in range(len(p1)):
             baby.append(random.choice((p1[i],p2[i])))
-        # print baby, "baby"
         return baby
 
     def mutate(self, child, mutation_rate):
@@ -111,34 +96,34 @@ class GA:
         new_pop = []
         i = 0
         while i < len(self.bots)-survival:
-            # print "?"
+
             p1 = self.pickFitParent()
             p2 = self.pickFitParent()
             child = self.crossover(p1, p2)
-            # print "??"
-            # print child
+
             child = self.mutate(child, mutation)
-            # print child
             new_pop.append(child)
             i += 1
-        # print new_pop, "!"
         bestN = self.pickBestN(survival)
-        # print bestN, "?!"
         for item in bestN:
             new_pop.append(item)
-        # print new_pop, "!!"
         self.bots = new_pop
 
-    def evolve(self, number_epochs, mutation = .01, min_mutation = .001, shrink = .95):
-        self.generateInitPopulation(2)
+    def evolve(self, number_epochs, pop_size, mutation = .01, min_mutation = .001, shrink = .95):
+        self.generateInitPopulation(pop_size)
         print("self.fitnesses: ", self.fitnesses)
         for i in range(number_epochs):
             self.generateNewPopulation(mutation)
             self.generatePopulationFitness()
             if mutation > min_mutation:
                 mutation *= shrink
+        max_index = self.fitnesses.index(max(self.fitnesses))
+        bestBot = ''.join(str(e)+' ' for e in self.bots[max_index])
+
         try:
-            open("weights.txt", "w").write("Weights: {0}")
+            fl=open("weights.txt", "w")
+            fl.write(bestBot)
+            fl.close()
         except:
             print("could not open")
             sys.exit(0)
@@ -146,32 +131,4 @@ class GA:
 
 if __name__=="__main__":
     ga = GA()
-    ga.evolve(100000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #
+    ga.evolve(10, 5)
