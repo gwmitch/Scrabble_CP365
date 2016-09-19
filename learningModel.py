acdf3390ffb6989b1835722d7de8eaee4e96ef48
kpontikes <katherinepontikes@gmail.com>
2016-09-19 12:07:53 -0600

    Add files via upload

--- learningModel.py
+++ learningModel.py
@@ -3,9 +3,13 @@
 import numpy as np
 import os, sys
 from os import listdir
+from ScrabbleGame import *
+from ScrabblePlayer import *
+from scrabble_globals import *
 
 
 class LearningModel:
+
 	def __init__(self, games):
 		self.games = games
 
@@ -21,7 +25,8 @@ class LearningModel:
 			#else:
 			#	count += 1 
 		total = total + count
-		print count
+		self.board = [0] * 6489
+		#print count
 		#print count
 		##self.games[0].printGame()
 
@@ -38,29 +43,94 @@ class LearningModel:
 		allMoves = []
 		for row in my_data:
 			moveContents = []
-			for x in range(len(row)):
+			for x in row:
 				for letter in x:
 					if letter.islower():
 						blank_tile = letter
-				x.replace(".", blank_tile)
-				moveContents.append(row[x])
+						x.replace(".", blank_tile)
+				moveContents.append(x)
 			currMove = Move(moveContents)
 			allMoves.append(currMove)
 		return allMoves
 
+	#def findStartingDirection(self):
+	#	self.first_move = self.games[0]
+	#	if self.first_move.position == "H8":
+	#		print "Cannot determine direction of starting move."
+	#	else:
+	#		self.parseMoveLocation(self.first_move)
+	#		is_vertical = TestMoveOnBoard.goesThroughCenter()
+	#	return is_vertical
+      
+	def changeLetterToNumber(self, letter):
+		letter_num = ord(letter) - 96
+		return letter_num
+
+	def parseMoveLocation(self, move):
+		col = self.changeLetterToNumber(move.position[0]) - 1
+		row = int(move.position[1] + move.position[2]) - 1
+
+		for letter in move.wordPlayed:
+			TestMoveOnBoard.move_dictionary[(row, col)]= str(letter)
+			self.addMoveToList(move, row, col)
+
+	def addMoveToList(self, move, row, col):
+		is_vertical = TestMoveOnBoard.isMoveVertical()
+		for letter in move.wordPlayed:
+			list_position = (row * 405) + col + self.changeLetterToNumber(letter)
+			self.board[list_position] = 1
+			if is_vertical == true:
+				col += 1
+			else:
+				row += 1
+
+	def parseWordBank(self, move):
+		slot = 0
+		for letter in move.wordBank:
+			list_position = 6075 + self.changeLetterToNumber(letter) + slot
+			self.board[list_position] = 1
+			slot += 27
+
+	def parseMoves(self):
+		for move in self.games:
+			self.parseMoveLocation(move)
+			self.parseWordBank(move)
+
+
+	def printBoard(self):
+		print self.board
+
 class Game:
+
 	def __init__( self, moves ): #right now everything is stored as a string 
 		self.moves = moves
 		self.numberMoves = len(moves)
-
-	def initializeBoard(self):
-		#self.board = [0 for col in range(405)] for row in range(15)]
-		self.board = [0] * 6264 #6489
 
 	def printGame(self):
 		for i in range(len(self.moves)):
 			print "move: "
 			self.moves[i].printMove()
+
+class TestMoveOnBoard(ScrabblePlayer):
+	
+	def inputMove(self):
+		self.move_dictionary = {}
+		#moves = []
+		for key, move in self.move_dictionary.iteritems():
+			if(self.game.boardWouldBeLegal(move)):
+				return move
+
+	def isMoveVertical(self):
+		if self.move_dictionary.keys()[1][0] != self.move_dictionary.keys()[0][0]:
+			return true
+		return false
+
+  	def goesThroughCenter(self):
+  		move = self.inputMove()
+  		if (7,7) in self.move_dictionary.keys():
+  			return true
+  		return false
+
 
 class Move:
 
@@ -72,41 +142,23 @@ class Move:
 		self.pointsGained = moveContents[4]
 		self.totalScore = moveContents[5]
 
-	def printMove(self):
-		moveString = "Player: " + self.player + " WordBank: " + self.wordBank + " Position: " + self.position + " WordPlayed: " + self.wordPlayed + " PointsGained: " + self.pointsGained + " TotalScore: " + self.totalScore
-		print moveString
+		#self.board = [0] * 6489
 
-	def findStartingDirection(self):
-		if self.position == "F8":
-			print "Cannot determine direction of starting move." #fix this by looking at next move
-		else:
+	#def initializeBoard(self):
+		#self.board = [0 for col in range(405)] for row in range(15)]
+		#self.board = [0] * 6489
 
-	def changeLetterToNumber(self, letter):
-		letter_num = ord(letter) - 96
-		return letter_num
+	#def printMoveBoard(self):
+	#	print self.board
 
-	def parseMoveLocation(self):
-		col = changeLetterToNumber(self.position[0]) - 1
-		row = int(self.position[1] + self.position[2]) - 1
-		is_vertical = findMoveDirection()
-		for letter in self.wordPlayed:
-			list_position = (row * 405) + col + changeLetterToNumber(letter)
-			self.board[list_position] = 1
-			if is_vertical == True:
-				col ++
-			else:
-				row ++
+	#def printMove(self):
+	#	moveString = "Player: " + self.player + " WordBank: " + self.wordBank + " Position: " + self.position + " WordPlayed: " + self.wordPlayed + " PointsGained: " + self.pointsGained + " TotalScore: " + self.totalScore
+	#	print moveString
 
-	def parseWordBank(self):
-		slot = 0
-		for letter in self.wordBank:
-			list_position = 6075 + changeLetterToNumber(letter) + slot
-			self.board[list_position] = 1
-			slot += 27
-
-	def findMoveDirection(self):
-		start = self.position
-		return direction
+	
+	#def findMoveDirection(self):
+	#	TestMoveOnBoard
+	#	return is_vertical
 
 def loadAllDataSets(dir_num):
 	games = os.listdir("GameData/" + str(dir_num) + "/")
@@ -115,6 +167,7 @@ def loadAllDataSets(dir_num):
 
 
 if __name__=="__main__":
+
     ##my_data = loadSingleDataset()
     ##currGame = Game(parseData(my_data))
 	global total
@@ -125,3 +178,5 @@ if __name__=="__main__":
 		model = LearningModel(emptyArr)
 		model.initialize(games, i)
 	print total
+	model.parseMoves()
+	model.printBoard()
